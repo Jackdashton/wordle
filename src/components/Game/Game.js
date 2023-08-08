f7cd26fbc7676b320d42579237797cf1704c5961
jack ashton <jackdashton@outlook.com>
2023-08-08 22:46:23 +0100

    added GuessList component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import Input from "./Input.js"
+import GuessList from "./GuessList.js";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,6 +13,7 @@ console.info({ answer });
 function Game() {
   return <>
   < Input />
+  < GuessList />
   </>;
 }
 
---
+++ src/components/Game/GuessList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GuessList() {
+  return (
+    <>
+      <h1>Previous Guesses</h1>
+      {/* Pass in guesses in to an array, then list the array, no key warnings, index for key is fine as order won't change */}
+    </>
+  );
+}
+
+export default GuessList;
